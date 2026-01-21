ad5f5c08cec41adc36dc27b10a00adb52a67f5c0
vvn-sequeira <svivianmarcel@gmail.com>
2026-01-21 23:32:12 +0530

    clodinary

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const {v2:cloudinary } = require("cloudinary") // image storage 
 const cors = require("cors")
 const bodyParser = require('body-parser')
 const {registrationMODLE} = require("./RegistrationModel.js")
@@ -24,3 +25,53 @@ const connectDB = async()=> {
     console.log("Failed to Connect to the DB , ", error)
   }
 }
+
+app.post("/register", async()=>{
+
+})
+
+
+const CloudImg = async(LocalFilePath)=>{
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY , 
+        api_secret:  process.env.CLOUDINARY_API_SECERET
+    });
+    
+    // Upload an image
+     const uploadResult = await cloudinary.uploader
+       .upload(
+           LocalFilePath, {
+            resource_type:'auto',
+           }
+       )
+       .catch((error) => {
+           console.log(error);
+           
+       });
+    
+    console.log(uploadResult);
+    
+    // Optimize delivery by resizing and applying auto-format and auto-quality
+    const optimizeUrl = cloudinary.url('shoes', {
+        fetch_format: 'auto',
+        quality: 'auto'
+    });
+    
+    console.log(optimizeUrl);
+    
+    // Transform the image: auto-crop to square aspect_ratio
+    const autoCropUrl = cloudinary.url('shoes', {
+        crop: 'auto',
+        gravity: 'auto',
+        width: 500,
+        height: 500,
+    });
+    
+    console.log("finalURL:",autoCropUrl); 
+    return autoCropUrl    
+}
+
+// CloudImg() 
